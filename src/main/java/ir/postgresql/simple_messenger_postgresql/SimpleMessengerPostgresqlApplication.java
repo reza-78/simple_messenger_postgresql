afb27e379147add73330c697e6bf6f445ac04cc6
reza mehtari <remecom99@gmail.com>
2022-06-05 11:24:56 +0430

    init

---
+++ src/main/java/ir/postgresql/simple_messenger_postgresql/SimpleMessengerPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package ir.postgresql.simple_messenger_postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMessengerPostgresqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMessengerPostgresqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/postgresql/simple_messenger_postgresql/SimpleMessengerPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.postgresql.simple_messenger_postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMessengerPostgresqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
